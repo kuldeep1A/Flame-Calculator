45ae7ca72053845677244cda65295f54f0660bc3
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-09 00:09:06 +0530

    start-complete 'btn7'

--- src/main/java/com/kuldeep1a/calculator/ui/CalculatorUI.java
+++ src/main/java/com/kuldeep1a/calculator/ui/CalculatorUI.java
@@ -142,6 +142,20 @@ public class CalculatorUI {
 
 
         btn7 = createButton("7", columns[0], rows[2]);
+        btn7.addActionListener(event -> {
+            if (addToDisplay){
+                if (Pattern.matches("[0]*", inputScreen.getText())){
+                    inputScreen.setText("7");
+                } else {
+                    inputScreen.setText(inputScreen.getText() + "7");
+                }
+            } else {
+                inputScreen.setText("7");
+                addToDisplay = true;
+            }
+            go = true;
+        });
+
         btn8 = createButton("8", columns[1], rows[2]);
         btn9 = createButton("9", columns[2], rows[2]);
         btnMul = createButton("*", columns[3], rows[2]);
