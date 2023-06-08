af8637e9e31d0d2f4e6842637070d5a261b61a7f
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-08 21:38:19 +0530

    complete the show structure of application

--- src/main/java/com/kuldeep1a/calculator/theme/ThemeLoader.java
+++ src/main/java/com/kuldeep1a/calculator/theme/ThemeLoader.java
@@ -1,7 +1,5 @@
 package com.kuldeep1a.calculator.theme;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.kuldeep1a.calculator.theme.properties.Theme;
@@ -22,7 +20,7 @@ public class ThemeLoader {
         mapper.findAndRegisterModules();
         try {
             ThemeList themeList = mapper.readValue(new File("src/main/resources/application.yaml"), ThemeList.class);
-
+            return themeList.getThemesAsMap();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/kuldeep1a/calculator/ui/CalculatorUI.java
+++ src/main/java/com/kuldeep1a/calculator/ui/CalculatorUI.java
@@ -1,10 +1,20 @@
 package com.kuldeep1a.calculator.ui;
 
 import com.kuldeep1a.calculator.theme.ThemeLoader;
+import com.kuldeep1a.calculator.theme.properties.Theme;
 
 import javax.swing.*;
+import java.util.Map;
 
 public class CalculatorUI {
+    private static final String DOUBLE_OR_NUMBER_REGEX = "([-]?\\d+[.]\\d*)|(\\d+)|(-\\d+)";
+    private static final String APPLICATION_TITLE = "Calculator";
+    private static final int WINDOW_WIDTH = 410;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final int BUTTON_WIDTH = 80;
+    private static final int BUTTON_HEIGHT = 70;
+    private static final int MARGIN_X = 20;
+    private static final int MARGIN_Y = 60;
     private final JFrame window;
     private static final String FONT_NAME = "Comic Sans MS";
     private JButton btnC;
@@ -29,10 +39,18 @@ public class CalculatorUI {
     private JButton btnRoot;
     private JButton btnPower;
     private JButton btnLog;
+    private final Map<String, Theme> themeMap;
 
     public CalculatorUI() {
         themeMap = ThemeLoader.loadThemes();
+        window = new JFrame();
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setLocationRelativeTo(null);
 
+        window.setLayout(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 
 
